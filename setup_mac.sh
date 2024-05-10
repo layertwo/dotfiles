74d231c0ea52884ea92dbba699d6fe94391f94f3
Lucas Messenger <1335960+layertwo@users.noreply.github.com>
2024-05-09 19:52:09 -0700

    chore: update setup_mac.sh

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# TODO add --personal flag to include additional packages
-# NOTE future cask ideas: airpass nightowl there
 
 # """ setup bash """
 cp ~/.bash_profile ~/.bash_profile.bak
@@ -9,30 +7,26 @@ cp bash_profile ~/.bash_profile
 # """ setup homebrew """
 # install homebrew
 xcode-select --install
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew doctor
 
 # install packages from homebrew
-brew install ansible ansible-lint cloc curl git go grep htop iproute2mac \
-             jq md5sha1sum nmap openssh openssl python3 rsync screen sshrc \
+brew install ansible ansible-lint awscli cloc curl git go grep htop iproute2mac \
+             jq kubernetes-cli md5sha1sum nmap openssh openssl python3 rsync screen \
              inetutils tmux vim watch wget xz zstd zsh
 
 # install packages from homebrew cask
-brew install caskroom/cask/brew-cask
-brew cask install visual-studio-code docker flux firefox gpg-suite keepassxc keepingyouawake \
-                  iterm2 postman the-unarchiver tresorit virtualbox vlc wireshark signal --force
+brew install --cask visual-studio-code docker firefox keepassxc keepingyouawake iterm2 \
+            standard-notes protonvpn the-unarchiver tresorit vlc wireshark signal --force
 
 # cleanup
 brew cleanup
-
-# install xcode-select
-xcode-select --install
 
 # """ setup git """
 cp gitconfig ~/.gitconfig
 
 # """ install visidata """
-pip3 install visidata dpkt lxml virtualenv
+pip3 install visidata virtualenv
 
 # """ setup vim """
 ./setup_vim.sh
