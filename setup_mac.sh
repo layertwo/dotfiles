096d05396ed302eb333ed62cc86565ab127fc9e6
Lucas Messenger <1335960+layertwo@users.noreply.github.com>
2024-05-11 13:14:57 -0700

    chore: add oh-my-zsh and update brew packages

--- setup_mac.sh
+++ setup_mac.sh
@@ -4,19 +4,23 @@
 cp ~/.bash_profile ~/.bash_profile.bak
 cp bash_profile ~/.bash_profile
 
+# setup oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # """ setup homebrew """
 # install homebrew
 xcode-select --install
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew doctor
 
 # install packages from homebrew
-brew install ansible ansible-lint awscli cloc curl git go grep htop iproute2mac \
-             jq kubernetes-cli md5sha1sum nmap openssh openssl python3 rsync screen \
-             inetutils tmux vim watch wget xz zstd zsh
+brew install ansible ansible-lint awscli cloc curl fluxcd/tap/flux git go grep htop \
+            iproute2mac jq kubernetes-cli md5sha1sum nmap openssh openssl python3 \
+            rsync screen inetutils tmux vim watch wget xz zstd zsh
+
 
 # install packages from homebrew cask
-brew install --cask visual-studio-code docker firefox keepassxc keepingyouawake iterm2 \
+brew install --cask visual-studio-code discord docker firefox keepassxc keepingyouawake iterm2 \
             standard-notes protonvpn the-unarchiver tresorit vlc wireshark signal --force
 
 # cleanup
